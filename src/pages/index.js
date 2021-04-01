3c94e590892a9b9ac586aa547cd05300d93d3f57
Matias Lorenzo <matiaslorenzo@Matiass-MacBook-Pro.local>
2021-04-02 00:34:22 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import GlobalStyle from '../globalStyles';
+
+import "@fontsource/permanent-marker" // Defaults to weight 400.
+
+
+
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(283.96deg, rgba(213, 29, 217, 0.5) 0%, rgba(220, 41, 202, 0) 100%), #252F84;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Title = styled.h1`
+  color: white;
+  font-family: Permanent Marker, cursive;
+`
+
+
+const IndexPage = () => (
+  <Main>
+    <GlobalStyle />
+    <Title>
+    COMING SOON
+    </Title>
+  </Main>
+)
+
+export default IndexPage
